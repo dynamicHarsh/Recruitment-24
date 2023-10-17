7525850010480338b36837584a0b04900c95af84
dynamicHarsh <2140154@sliet.ac.in>
2023-10-18 02:44:23 +0530

    updated success

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -11,8 +11,9 @@ function Success() {
         <h2 style={{ marginBottom: "2rem" }}>
          Congrats!! You have Registered Successfully
         </h2>
-        <h3>Kindly sign up for the contest in <a className=" hackerrank" href="https://www.hackerrank.com/ssdc-recruitment24">Hackerrank</a></h3>
-        <h3>Join our <a className=" whatsapp" href="https://chat.whatsapp.com/IuLJV0FNTey5yk7t15QI5p">WhatsApp</a> Group for further updates</h3>
+        
+        <h3>Kindly Join our <a className=" whatsapp" href="https://chat.whatsapp.com/IuLJV0FNTey5yk7t15QI5p">WhatsApp</a> Group for further updates</h3>
+        <h3>Sign Up for the contest in <a className=" hackerrank" href="https://www.hackerrank.com/ssdc-recruitment24">Hackerrank</a></h3>
         
       </div>
       </div>
